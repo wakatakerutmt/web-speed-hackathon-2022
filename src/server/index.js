2320f320877b16483960c0adabe17f5908be443c
wakatakerutmt <desuapostle@gmail.com>
2022-11-24 23:12:00 +0900

    ブラウザキャッシュcache-controlの有効化

--- src/server/index.js
+++ src/server/index.js
@@ -38,7 +38,6 @@ server.addHook("onRequest", async (req, res) => {
 });
 
 server.addHook("onRequest", async (req, res) => {
-  res.header("Cache-Control", "no-cache, no-store, no-transform");
   res.header("Connection", "close");
 });
 
