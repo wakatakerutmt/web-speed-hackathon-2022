4b28b07388f6058fc98745c1d1966296ca11f8d3
wakatakerutmt <desuapostle@gmail.com>
2022-11-26 16:47:27 +0900

    svg

--- src/client/foundation/pages/races/Odds/Odds.jsx
+++ src/client/foundation/pages/races/Odds/Odds.jsx
@@ -93,10 +93,8 @@ export const Odds = () => {
         <Spacer mt={Space * 4} />
 
         <Callout $closed={isRaceClosed}>
-          {/* <FontAwesomeIcon icon={faInfoCircle} /> */}
-          <svg aria-hidden="true" className="svg-inline--fa fa-circle-info " data-icon="circle-info" data-prefix="fas" focusable="false" role="img" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-            <path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-144c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z" fill="currentColor">
-            </path>
+          <svg aria-hidden="true" className="svg-inline--fa fa-info-circle fa-w-16" data-fa-i2svg="" data-icon="info-circle" data-prefix="fas" focusable="false" role="img" style={{height: '1em', verticalAlign:'-0.125em', width: '1.125em'}} viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+            <path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z" fill="currentColor"></path>
           </svg>
           {isRaceClosed
             ? "このレースの投票は締め切られています"
--- src/client/foundation/pages/races/RaceResult/internal/BettingTicketList/BettingTicketList.jsx
+++ src/client/foundation/pages/races/RaceResult/internal/BettingTicketList/BettingTicketList.jsx
@@ -60,11 +60,8 @@ export const BettingTicketList = ({ children }) => {
   if (React.Children.count(children) === 0) {
     return (
       <Placeholder>
-        {/* <FontAwesomeIcon icon={faTicketAlt} /> */}
-        <svg aria-hidden="true" className="svg-inline--fa fa-ticket-simple " data-icon="ticket-simple" data-prefix="fas" focusable="false" role="img" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 128C0 92.7 28.7 64 64 64H512c35.3 0 64 28.7 64 64v80c-26.5 0-48 21.5-48 48s21.5 48 48 48v80c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V304c26.5 0 48-21.5 48-48s-21.5-48-48-48V128z" fill="currentColor">
-          </path>
-        </svg>
+        <svg aria-hidden="true" data-fa-i2svg="" data-icon="ticket-alt" data-prefix="fas" focusable="false" role="img" style={{height: '1em', verticalAlign:'-0.125em', width: '1.125em'}} viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg">
+          <path d="M128 160h320v192H128V160zm400 96c0 26.51 21.49 48 48 48v96c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48v-96c26.51 0 48-21.49 48-48s-21.49-48-48-48v-96c0-26.51 21.49-48 48-48h480c26.51 0 48 21.49 48 48v96c-26.51 0-48 21.49-48 48zm-48-104c0-13.255-10.745-24-24-24H120c-13.255 0-24 10.745-24 24v208c0 13.255 10.745 24 24 24h336c13.255 0 24-10.745 24-24V152z" fill="currentColor"></path></svg>
         <div>購入した拳券はありません</div>
       </Placeholder>
     );
