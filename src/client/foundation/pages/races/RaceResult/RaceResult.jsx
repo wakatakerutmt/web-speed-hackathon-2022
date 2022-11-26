b3b4923e65b5d7c59a635ab3034182ee156481ca
wakatakerutmt <desuapostle@gmail.com>
2022-11-27 02:10:35 +0900

    styleつけ忘れ

--- src/client/foundation/pages/races/RaceResult/RaceResult.jsx
+++ src/client/foundation/pages/races/RaceResult/RaceResult.jsx
@@ -58,7 +58,7 @@ export const RaceResult = () => {
       <Section dark shrink>
         <LiveBadge>Live</LiveBadge>
         <Spacer mt={Space * 2} />
-        <img height="225"  loading="eager" src={srcUrl} width="400" />
+        <img height="225"  loading="eager" src={srcUrl} style={{height: "auto", width: "100%"}} width="400" />
       </Section>
 
       <Spacer mt={Space * 2} />
